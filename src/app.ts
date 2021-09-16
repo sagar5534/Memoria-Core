2decb301fec50190c13f58529dc632819f7012a2
Sagar Patel <s.72427patel@gmail.com>
2021-09-16 01:24:40 -0400

    Added NAT PMP

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,38 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 // import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import routes from './routes';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import routes from "./routes";
+import { boolean } from "webidl-conversions";
+// tslint:disable-next-line
+const punch = require('holepunch');
 
 dotenv.config();
 
-const app: Express = express()
+const app: Express = express();
 const PORT = process.env.PORT || 3000;
-const DB_PATH = process.env.DB_PATH
+const DB_PATH = process.env.DB_PATH;
+const REMOTE_ACCESS = (process.env.REMOTE_ACCESS === 'true');
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-mongoose.connect(DB_PATH)
+mongoose.connect(DB_PATH);
+
+// Setting up nat pmp
+
+if (REMOTE_ACCESS) {
+  punch({
+    debug: false,
+    mappings: [{ internal: PORT, external: 3001, secure: false }],
+    upnp: true,
+    pnp: true,
+  });
+}
 
 app.listen(PORT, () => {
   // tslint:disable-next-line:no-console
-  console.log(`Running on ${PORT} ⚡`)
+  console.log(`Running on ${PORT} ⚡`);
 });
