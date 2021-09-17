b56a21331f2f90590dcdbbcf60c3496518c2647e
Sagar Patel <s.72427patel@gmail.com>
2021-09-17 01:43:14 -0400

    Added users endpoint funcs

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -9,22 +9,39 @@ export class UserController {
   }
 
   async find(req: Request, res: Response) {
-    res.send(await service.findAll());
-    // return await service.findOne(id);
+    if (!req.params.id) return res.sendStatus(400);
+    try {
+      res.send(await service.findOne(req.params.id));
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 
   async create(req: Request, res: Response) {
-    res.send(await service.findAll());
-    // return await service.create(user);
+    if (!req.body.user) return res.sendStatus(400);
+    try {
+      res.send(await service.create(req.body.user));
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 
   async update(req: Request, res: Response) {
-    res.send(await service.findAll());
-    // return await service.update(id, updateUser);
+    if (!req.params.id) return res.sendStatus(400);
+    if (!req.body.updatedUser) return res.sendStatus(400);
+    try {
+      res.send(await service.update(req.params.id, req.body.updatedUser));
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 
   async delete(req: Request, res: Response) {
-    res.send(await service.findAll());
-    // return await service.delete(id);
+    if (!req.params.id) return res.sendStatus(400);
+    try {
+      res.send(await service.delete(req.params.id));
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,5 +5,9 @@ const userRoutes = Router();
 const userController = new UserController();
 
 userRoutes.get("/", userController.index);
+userRoutes.get("/:id", userController.find);
+userRoutes.post("/", userController.create);
+userRoutes.put("/:id", userController.update);
+userRoutes.delete("/:id", userController.delete);
 
 export default userRoutes;
