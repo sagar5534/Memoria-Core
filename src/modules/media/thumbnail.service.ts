e00db410c92a314851f9151cb08858c1c550b265
Sagar Patel <s.72427patel@gmail.com>
2022-01-09 18:10:45 -0500

    Generate Gifs for Videos

--- src/modules/media/thumbnail.service.ts
+++ src/modules/media/thumbnail.service.ts
@@ -1,10 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { Injectable } from '@nestjs/common';
 import * as imageThumbnail from 'image-thumbnail';
 import * as fs from 'fs';
 import { Media } from 'src/models/media.model';
 import { join } from 'path';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+import ThumbnailGenerator from 'video-thumbnail-generator';
 const config = require('config');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffprobePath = require('@ffprobe-installer/ffprobe').path;
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
+
+const mediaType = {
+  photo: 1,
+  video: 2,
+};
 
 @Injectable()
 export class ThumbnailService {
@@ -28,8 +40,24 @@ export class ThumbnailService {
     );
 
     try {
-      const thumbnail = await imageThumbnail(inputPath, this.imgOptions);
-      return this.saveThumbnail(thumbnail, savePath);
+      if (mediaDocument.mediaType === mediaType.video) {
+        const tg = new ThumbnailGenerator({
+          sourcePath: inputPath,
+          thumbnailPath: this.saveLocation,
+          tmpDir: this.saveLocation,
+          percent: '25%',
+        });
+
+        return await tg.generateGif({
+          fps: 0.75,
+          scale: 180,
+          speedMultiple: 4,
+          deletePalette: true,
+        });
+      } else {
+        const thumbnail = await imageThumbnail(inputPath, this.imgOptions);
+        return this.saveThumbnail(thumbnail, savePath);
+      }
     } catch (err) {
       console.error(err);
     }
